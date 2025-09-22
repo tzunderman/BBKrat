f9e5708191ed384a123a1a8aab944b0927f9b98b
Timo Zunderman <T.R.Zunderman@student.tudelft.nl>
2025-09-22 08:18:46 +0200

    Changed the controller mapping to work with Linux and added some more modes

--- BBKrat.py
+++ BBKrat.py
@@ -8,25 +8,29 @@ import serial.tools.list_ports as stlp
 # Axes
 AXIS_L_STICK_X = 0
 AXIS_L_STICK_Y = 1
-AXIS_R_STICK_X = 2
-AXIS_R_STICK_Y = 3
+AXIS_R_STICK_X = 3
+AXIS_R_STICK_Y = 4
 
-# D-pad buttons
-BTN_DPAD_UP = 11
-BTN_DPAD_DOWN = 12
+# # Shoulder buttons
+BTN_SHOULDER_UP = 5
+BTN_SHOULDER_DOWN = 4
+# # D-pad buttons
+HAT_DPAD_Y = 1
 
 
 #Demo mode to see which button index of the controller is what.
 BTN_INDEX_DEMO_MODE = False
-
+# Print mode to print data in the terminal instead of sending it to an Arduino
+PRINT_MODE = False
 # ---------------- Initial variable values ----------------
 
 # Max power
 max_power = 20  # 20% start
 
-# Prev button values
-prev_dpad_up = False
-prev_dpad_down = False
+# # Prev button values
+prev_shoulder_up = False
+prev_shoulder_down = False
+prev_dpad = 0
 
 # ---------------- Init ----------------
 pygame.init()
@@ -42,18 +46,27 @@ print(f"Connected to: {joystick.get_name()}")
 
 
 while BTN_INDEX_DEMO_MODE:
+    pygame.event.pump()
+
     axes = [joystick.get_axis(i) for i in range(joystick.get_numaxes())]
     buttons = [joystick.get_button(i) for i in range(joystick.get_numbuttons())]
     hats = [joystick.get_hat(i) for i in range(joystick.get_numhats())]
-    print(f"Axes: {axes}  Buttons: {buttons}  Hats: {hats}") 
+    print(f"Axes: {axes}  Buttons: {buttons}  Hats: {hats}", end='\r') 
 
 # ---------------- Serial connection ----------------
-port = [port.device for port in stlp.comports() if not port.manufacturer is None and "Arduino" in port.manufacturer][0]
-# print(port)
-ser = serial.Serial(port, 115200)
-# For some reason there needs to be a delay in order for the Arduino to be
-# ready to receive messages 
-time.sleep(1)
+ser: serial.Serial | None = None
+if not PRINT_MODE:
+    connected_arduinos = [port.device for port in stlp.comports() if not port.manufacturer is None and "Arduino" in port.manufacturer]
+    if len(connected_arduinos) == 0:
+        print("No Arduino connected")
+        exit()
+        
+    port = connected_arduinos[0]
+    # print(port)
+    ser = serial.Serial(port, 115200)
+    # For some reason there needs to be a delay in order for the Arduino to be
+    # ready to receive messages 
+    time.sleep(1)
 
 # ---------------- Main Loop ----------------
 try:
@@ -63,21 +76,22 @@ try:
         # ----- Read controller -----
         leftStickX = joystick.get_axis(AXIS_L_STICK_X)
         rightStickY = joystick.get_axis(AXIS_R_STICK_Y)
-        current_dpad_up = joystick.get_button(BTN_DPAD_UP)
-        current_dpad_down = joystick.get_button(BTN_DPAD_DOWN)
-
+        current_shoulder_up = joystick.get_button(BTN_SHOULDER_UP)
+        current_shoulder_down = joystick.get_button(BTN_SHOULDER_DOWN)
+        current_dpad = joystick.get_hat(0)[HAT_DPAD_Y]
 
 
         # ----- Process controller inputs -----
         
         # Change max power with d-pad buttons (steps of 10%)
-        if current_dpad_up and not prev_dpad_up:
+        if current_shoulder_up and not prev_shoulder_up or current_dpad == 1 and prev_dpad != 1:
             max_power += 10
-        if current_dpad_down and not prev_dpad_down:
+        if current_shoulder_down and not prev_shoulder_down or current_dpad == -1 and prev_dpad != -1:
             max_power -= 10
 
-        prev_dpad_up = current_dpad_up
-        prev_dpad_down = current_dpad_down
+        prev_shoulder_up = current_shoulder_up
+        prev_shoulder_down = current_shoulder_down
+        prev_dpad = current_dpad
 
         # Clamp max power between 10% and 100%
         max_power = max(10, min(100, max_power))
@@ -95,21 +109,25 @@ try:
         pwmRight = max(1, round((powerRight + 100) * 255 / 200))
 
         # ----- Communicate results to the Arduino 
-        # print(
-        #     f"Drive: {drive:>4}%  Steer: {steer:>4}%  MaxPower: {int(max_power):>3}%  "
-        #     f"PowerL: {powerLeft:>4}%  PowerR: {powerRight:>4}%  "
-        #     f"PWML: {pwmLeft:>5.1f}  PWMR: {pwmRight:>5.1f}",
-        #     end="\r"
-        # )
-        buf = [pwmLeft, pwmRight, 0]
-        ser.write(bytes(buf))
-        b = ser.read_until()
-        if b != b"":
-            print(b, end='\r')
+        if PRINT_MODE:
+            print(
+                f"Drive: {drive:>4}%  Steer: {steer:>4}%  MaxPower: {int(max_power):>3}%  "
+                f"PowerL: {powerLeft:>4}%  PowerR: {powerRight:>4}%  "
+                f"PWML: {pwmLeft:>5.1f}  PWMR: {pwmRight:>5.1f}",
+                end="\r"
+            )
+        elif ser is not None:
+            buf = [pwmLeft, pwmRight, 0]
+            ser.write(bytes(buf))
+            b = ser.read_until()
+            if b != b"":
+                print(b, end='\r')
 
-        # Small delay to prevent too much spamming on the serial connection
-        time.sleep(0.02)
+            # Small delay to prevent too much spamming on the serial connection
+            time.sleep(0.02)
 except Exception as e:
+    print(e)
+
+if not PRINT_MODE and ser is not None:
     # Make sure to properly close the connection
     ser.close()
-    print(e)
