8f84e62524ed48bd9db75b414998163d71a59841
Renzo Ligthart <renzo@fortuin.nl>
2026-05-28 22:45:30 +0200

    Added 0.5*vcc offset to current readout

--- arduino_code/motor_controller/src/battery.cpp
+++ arduino_code/motor_controller/src/battery.cpp
@@ -1,11 +1,15 @@
 #include "battery.h"
 
+#define Vref 5.0
+
 #define R1 33.2e3
 #define R2 65.8e3
 
+#define dVdI 40e-3
+
 float read_voltage(uint8_t pin) {
     int analog_value = analogRead(pin);
-    float pin_voltage = (((float) analog_value) / 1023) * 5.0;
+    float pin_voltage = (((float) analog_value) / 1023.0) * Vref;
     float battery_voltage = pin_voltage * (R1 + R2) / R1;
 
     return battery_voltage;
@@ -13,8 +17,8 @@ float read_voltage(uint8_t pin) {
 
 float read_current(uint8_t pin) {
     int analog_value = analogRead(pin);
-    float pin_voltage = (((float) analog_value) / 1023.0) * 5.0;
-    float battery_current = pin_voltage / 40e-3;
+    float pin_voltage = (((float) analog_value) / 1023.0) * Vref;
+    float battery_current = (pin_voltage - 0.5*Vref) / dVdI;
 
     return battery_current;
 }
